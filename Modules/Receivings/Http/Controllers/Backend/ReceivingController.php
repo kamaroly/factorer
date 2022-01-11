9f708be5c082175bf47281777af510e2c28d10a4
KAMARO <kamaroly@gmail.com>
2022-01-11 09:13:51 +0300

    Purchase & Receiving indexes can be displayed

--- Modules/Receivings/Http/Controllers/Backend/ReceivingController.php
+++ Modules/Receivings/Http/Controllers/Backend/ReceivingController.php
@@ -2,78 +2,46 @@
 
 namespace Modules\Receivings\Http\Controllers\Backend;
 
-use Illuminate\Contracts\Support\Renderable;
+use App\Authorizable;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
+use Illuminate\Contracts\Support\Renderable;
 
 class ReceivingController extends Controller
 {
+    // use Authorizable;
+
+    public function __construct()
+    {
+        // Page Title
+        $this->module_title = 'Receiving';
+
+        // module name
+        $this->module_name = 'receivings';
+
+        // module icon
+        $this->module_icon = 'fas fa-cart';
+
+        // module model name, path
+        $this->module_model = "Modules\Receivings\Entities\Bill";
+    }
+
     /**
      * Display a listing of the resource.
      * @return Renderable
      */
     public function index()
     {
-        return view('receivings::index');
+        return view(
+            "receivings::backend.{$this->module_name}.index_datatable",
+            ['module_title' => $this->module_title,
+             'module_name' => $this->module_name,
+             'module_icon' => $this->module_icon,
+             'module_name_singular' => Str::singular($this->module_name),
+             'module_action' => 'List',
+             ]
+        );
     }
 
-    /**
-     * Show the form for creating a new resource.
-     * @return Renderable
-     */
-    public function create()
-    {
-        return view('receivings::create');
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     * @param Request $request
-     * @return Renderable
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Show the specified resource.
-     * @param int $id
-     * @return Renderable
-     */
-    public function show($id)
-    {
-        return view('receivings::show');
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     * @param int $id
-     * @return Renderable
-     */
-    public function edit($id)
-    {
-        return view('receivings::edit');
-    }
-
-    /**
-     * Update the specified resource in storage.
-     * @param Request $request
-     * @param int $id
-     * @return Renderable
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     * @param int $id
-     * @return Renderable
-     */
-    public function destroy($id)
-    {
-        //
-    }
 }
