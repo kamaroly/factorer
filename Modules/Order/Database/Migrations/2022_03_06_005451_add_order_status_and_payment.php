f0b1e2ef54b832d8a117e39a76c9f9f7b83238ba
KAMARO <kamaroly@gmail.com>
2022-03-05 22:39:22 +0300

    Added order status

---
+++ Modules/Order/Database/Migrations/2022_03_06_005451_add_order_status_and_payment.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddOrderStatusAndPayment extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->string("status")->default("processing");
+            $table->timestamp('paid_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('orders', function (Blueprint $table) {
+
+        });
+    }
+}
