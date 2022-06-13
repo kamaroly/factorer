58afd2c2d5e11c4e5efdf9e1646e0b3b89a8bd46
Kamaro <kamaroly@gmail.com>
2022-06-13 20:05:05 +0300

    Added route and the view for the accounting transaction

--- Modules/Accounting/Routes/web.php
+++ Modules/Accounting/Routes/web.php
@@ -28,4 +28,8 @@ Route::group(['namespace' => '\Modules\Accounting\Http\Controllers\Backend', 'as
     Route::get("$module_name/trashed", ['as' => "$module_name.trashed", 'uses' => "$controller_name@trashed"]);
     Route::patch("$module_name/trashed/{id}", ['as' => "$module_name.restore", 'uses' => "$controller_name@restore"]);
     Route::resource("$module_name", "$controller_name");
+
+
 });
+
+Route::view('/accounting/transaction', "accounting::backend.accounting");
