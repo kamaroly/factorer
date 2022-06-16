2fc27595d9acd821aebf93bacae71879bbe91808
Kamaro <kamaroly@gmail.com>
2022-06-16 22:36:44 +0300

    Added layout to the new accounting form

--- Modules/Accounting/Resources/views/backend/accounting/accounting.blade.php
+++ Modules/Accounting/Resources/views/backend/accounting/accounting.blade.php
@@ -1,1 +1,24 @@
-Accounting Test
+@extends('backend.layouts.app')
+
+@section('title')
+    Accounting Transactions
+@endsection
+
+
+@section('breadcrumbs')
+<x-backend-breadcrumbs>
+    <x-backend-breadcrumb-item type="active" icon='money'>Accounting</x-backend-breadcrumb-item>
+</x-backend-breadcrumbs>
+@endsection
+
+
+
+@section('content')
+<div class="card">
+    <div class="card-body">
+
+        <h1>The form for accounting to be inserted here</h1>
+
+    </div>
+</div>
+@endsection
